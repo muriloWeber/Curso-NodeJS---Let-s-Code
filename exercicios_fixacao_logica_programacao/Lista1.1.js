30b1fb5c028e3f24316cb4fe1c54304b204bcd78
Murilo de Souza Weber <96751086+muriloWeber@users.noreply.github.com>
2021-12-30 20:17:07 -0300

    update erro de escrita

--- exercicios_fixacao_logica_programacao/Lista1.1.js
+++ exercicios_fixacao_logica_programacao/Lista1.1.js
@@ -6,7 +6,7 @@ function areaTerreno(ladoA, ladoB) {
     if(isNaN(ladoA) || isNaN (ladoB)) throw "Os lados do terreno devem ser números";
     if(ladoA === ladoB) throw "A imobiliária vende somente terrenos retangulares, que por sua vez, possuem lados diferentes)";
 
-    return console.log(`O terreno poussui ${ladoA * ladoB}m².`);
+    return console.log(`O terreno possui ${ladoA * ladoB}m².`);
 }
 
 const area1 = areaTerreno(3,4);
