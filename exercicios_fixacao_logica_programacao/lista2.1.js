7f73f38a8cac357fd26a87f0376ea5fb6920b2e9
muriloWeber <caboeb@gmail.com>
2022-01-13 13:52:58 -0300

    novo exercicio (lista2.1) de logica

---
+++ exercicios_fixacao_logica_programacao/lista2.1.js
@@ -0,0 +1,11 @@
+/*
+Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+*/
+
+const a = 1;
+
+const b = 2;
+
+const c = 5;
+
+(a + b < c) ? console.log(`A soma de A + B é menor que C.`) : console.log(`A soma de A + B não é menor que C.`)
